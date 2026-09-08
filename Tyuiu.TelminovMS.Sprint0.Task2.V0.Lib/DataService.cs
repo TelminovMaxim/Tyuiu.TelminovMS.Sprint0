4dd1015a3908008db682aeadad08a958e07b3433
TelminovMS <mtelminov02@gmail.com>
2026-09-08 23:51:12 +0500

    Task 2

---
+++ Tyuiu.TelminovMS.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.TelminovMS.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+
+        public static string GetMessage(string name)
+        {
+            return $"Привет, {name}";
+        }
+
+
+    }
+}
