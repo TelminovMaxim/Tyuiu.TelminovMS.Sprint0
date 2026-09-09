76d53ce8f76df95599b46d6eb420d97369d85d99
TelminovMS <mtelminov02@gmail.com>
2026-09-09 19:02:35 +0500

    Task3

---
+++ Tyuiu.TelminovMS.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.TelminovMS.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Tyuiu.TelminovMS.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.TelminovMS.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
