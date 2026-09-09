bd5cfa2f73a0cdfcacf63b34b727cdb5ee903987
TelminovMS <mtelminov02@gmail.com>
2026-09-09 22:49:31 +0500

    Task5

---
+++ Tyuiu.TelminovMS.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿namespace Tyuiu.TelminovMS.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static double Division(int x, int y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Переменная В = {0} на ноль дерлить нельзя", y);
+                return -1;
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+    }
+}
