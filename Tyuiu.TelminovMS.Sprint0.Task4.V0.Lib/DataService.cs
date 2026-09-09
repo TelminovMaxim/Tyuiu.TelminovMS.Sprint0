60d572173484dbfd508ebb8d07dae1f8b195670c
TelminovMS <mtelminov02@gmail.com>
2026-09-09 22:16:08 +0500

    Task4

---
+++ Tyuiu.TelminovMS.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿namespace Tyuiu.TelminovMS.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static double Division(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Tyuiu.TelminovMS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TelminovMS.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
