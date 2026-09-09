09d691731e29c11fee0ccbff4a546acb0ab4a533
TelminovMS <mtelminov02@gmail.com>
2026-09-10 00:52:54 +0500

    Task6

---
+++ Tyuiu.TelminovMS.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.TelminovMS.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var number = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(number);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void CheckSubstractionArrayValid()
+        {
+            var number = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstracyionArray(number);
+            Assert.AreEqual(-15, res);
+        }
+
+        [TestMethod]
+        public void CheckMultiplicationArrayValid()
+        {
+            var number = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(number);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
---
+++ Tyuiu.TelminovMS.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.TelminovMS.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.TelminovMS.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine("Сумма элементов масива равна: " + DataService.AdditionArray(numbers));
+            Console.WriteLine("Разность элементов масива равна: " + DataService.SubstracyionArray(numbers));
+            Console.WriteLine("Произведение элементов масива равна: " + DataService.MultiplicationArray(numbers));
+            Console.ReadKey();
+        }
+    }
+}
