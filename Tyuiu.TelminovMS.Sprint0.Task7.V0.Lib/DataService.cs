bcca395ca265beb664b15494bdb5e52cb1ef080a
Sie-mt <mtelminov02@gmail.com>
2026-09-12 18:44:28 +0500

    Task7

---
+++ Tyuiu.TelminovMS.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Tyuiu.TelminovMS.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+
+            for (var i = 0; i < numOne.Length; i++)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+
+            return resultArray;
+        }
+    }
+}
